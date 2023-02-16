a6fc25a72d84c7c21bda458fa3b619508a3acb67
Haopeng Zeng <andyzhp234@gmail.com>
2023-02-15 23:05:08 -0800

    update server log

--- backend/middlewares/socketCheckAuth.js
+++ backend/middlewares/socketCheckAuth.js
@@ -1,6 +1,6 @@
 // An middleware function that checks users session and make sure user is authenticated
 const socketCheckAuth = (socket, next) => {
-  console.log(socket.request.session);
+  console.log("checking auth: ", socket.request.session);
   if (!socket.request.session.isAuthenticated) {
     const socketError = new Error("User not authenticated");
     return next(socketError);
