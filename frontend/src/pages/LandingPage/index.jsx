3d5ffd19266e2eeaf34b18c4f43b97b21536fcb2
Haopeng Zeng <andyzhp234@gmail.com>
2023-02-16 16:47:36 -0800

    update mobile responsive

--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -3,25 +3,31 @@ import { useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
   const navigate = useNavigate();
+  const [height, setHeight] = React.useState(window.innerHeight);
 
+  React.useEffect(() => {
+    const handleResize = () => setHeight(window.innerHeight);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
-    <div className="absolute inset-0">
-      <div className="flex h-screen w-screen items-center justify-center bg-[url('../assets/bg-snowy-mint.jpg')] bg-cover p-7">
-        <div className="flex w-30rem flex-col items-center text-center">
-          <h1 className="text-5xl font-semibold sm:text-7xl">
-            <span className="landing-text-gradient">Chat</span> Anytime,
-            Anywhere
-          </h1>
-          <h2 className="mt-8 text-gray-700">
-            Say goodbye to the hassle of setting up complicated video calls and
-            struggling with unreliable messaging apps.
-          </h2>
-          <div
-            className="mt-8 cursor-pointer rounded-xl bg-emerald-400 px-5 py-3 font-semibold text-white hover:bg-emerald-500"
-            onClick={() => navigate("/login")}
-          >
-            Get Started
-          </div>
+    <div
+      className="flex h-screen w-screen items-center justify-center bg-[url('../assets/bg-snowy-mint.jpg')] bg-cover p-7"
+      style={{ height: height }}
+    >
+      <div className="flex w-30rem flex-col items-center text-center">
+        <h1 className="text-5xl font-semibold sm:text-7xl">
+          <span className="landing-text-gradient">Chat</span> Anytime, Anywhere
+        </h1>
+        <h2 className="mt-8 text-gray-700">
+          Say goodbye to the hassle of setting up complicated video calls and
+          struggling with unreliable messaging apps.
+        </h2>
+        <div
+          className="mt-8 cursor-pointer rounded-xl bg-emerald-400 px-5 py-3 font-semibold text-white hover:bg-emerald-500"
+          onClick={() => navigate("/login")}
+        >
+          Get Started
         </div>
       </div>
     </div>
