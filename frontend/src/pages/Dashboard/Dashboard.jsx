f3de94c6031a88f566c0398e5802170258d185de
Haopeng Zeng <andyzhp234@gmail.com>
2023-02-18 01:38:30 -0800

    update mobile responsive

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -19,13 +19,12 @@ export default function Dashboard() {
     // }
     // window.addEventListener("resize", handleResize);
     // return () => window.removeEventListener("resize", handleResize);
-    function appHeight() {
-      const doc = document.documentElement;
-      doc.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
-    }
-
-    window.addEventListener("resize", appHeight);
-    appHeight();
+    // function appHeight() {
+    //   const doc = document.documentElement;
+    //   doc.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+    // }
+    // window.addEventListener("resize", appHeight);
+    // appHeight();
   }, []);
 
   return (
