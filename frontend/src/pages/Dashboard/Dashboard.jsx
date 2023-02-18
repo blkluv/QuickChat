46b5a87dfffd81caf73e3e6432125cf2c8a9e7a5
Haopeng Zeng <andyzhp234@gmail.com>
2023-02-18 02:33:27 -0800

    update mobile responsive

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -7,18 +7,25 @@ import MessageDetailSideBar from "./MessageDetailSideBar/MessageDetailSideBar";
 export default function Dashboard() {
   const [openSideBar, setOpenSideBar] = React.useState(false);
   const [openDetailSideBar, setOpenDetailSideBar] = React.useState(false);
+  const [height, setHeight] = React.useState(window.innerHeight);
+
+  React.useEffect(() => {
+    connectWithSocketServer();
+  }, []);
 
   return (
-    <div className="relative flex h-screen flex-1 overflow-hidden">
-      <SideBar openSideBar={openSideBar} setOpenSideBar={setOpenSideBar} />
-      <MessageContent
-        setOpenSideBar={setOpenSideBar}
-        setOpenDetailSideBar={setOpenDetailSideBar}
-      />
-      <MessageDetailSideBar
-        openDetailSideBar={openDetailSideBar}
-        setOpenDetailSideBar={setOpenDetailSideBar}
-      />
+    <div className="absolute inset-0">
+      <div className="relative flex h-screen max-h-full flex-1 overflow-hidden">
+        <SideBar openSideBar={openSideBar} setOpenSideBar={setOpenSideBar} />
+        <MessageContent
+          setOpenSideBar={setOpenSideBar}
+          setOpenDetailSideBar={setOpenDetailSideBar}
+        />
+        <MessageDetailSideBar
+          openDetailSideBar={openDetailSideBar}
+          setOpenDetailSideBar={setOpenDetailSideBar}
+        />
+      </div>
     </div>
   );
 }
